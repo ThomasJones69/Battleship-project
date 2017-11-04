47d56b1ec52dc2d497828e2070e3c3b1495a58e9
ThomasJones69 <tjones@ziggo.nl>
2017-11-04 21:11:34 +0100

    Battleship project. Deel code van Khaldoon

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+        
+        <meta charset="UTF-8">
+        
+        <title>Battleship</title>
+
+        <link rel="stylesheet" type="text/css" href="Battelship.css">
+                
+</head>
+
+    <body>
+
+        <center>
+                <h1>Welkom to Battleship</h1>	
+
+                <p style="color:white">To find a ship, click to shoot at it!</p>
+
+                <div id="gameboard"> </div>
+
+	
+	<script type="text/javascript" src="Battelship.js"></script>
+    </body>
+        </center>
+</html>
