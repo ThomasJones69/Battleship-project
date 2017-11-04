0ac4c5e75a33f6f7684a495a3b1ecba17ff371ad
ThomasJones69 <tjones@ziggo.nl>
2017-11-04 22:13:58 +0100

    Reposition ships and format cleanup

--- index.php
+++ index.php
@@ -1,25 +1,26 @@
 <html>
-<head>
-        
+    <head>
+
         <meta charset="UTF-8">
-        
+
         <title>Battleship</title>
 
         <link rel="stylesheet" type="text/css" href="Battelship.css">
-                
-</head>
+
+    </head>
 
     <body>
 
-        <center>
-                <h1>Welkom to Battleship</h1>	
+    <center>
+        <h1>Welkom to Battleship</h1>	
 
-                <p style="color:white">To find a ship, click to shoot at it!</p>
+        <p style="color:white">To find a ship, click to shoot at it!</p>
 
-                <div id="gameboard"> </div>
+        <div id="gameboard"> </div>
 
-	
-	<script type="text/javascript" src="Battelship.js"></script>
+        <script type="text/javascript" src="Battelship.js"></script>
+    </center>
+    
     </body>
-        </center>
+
 </html>
