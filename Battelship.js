ede3491e5ca316c45bae0e8ca6a06075eaf3758d
ThomasJones69 <tjones@ziggo.nl>
2017-11-04 21:39:56 +0100

    changed grid squares to circles

--- Battelship.js
+++ Battelship.js
@@ -6,74 +6,70 @@ var gameBoardContainer = document.getElementById("gameboard");
 
 for (c = 0; c < cols; c++)
 
+{
+    for (r = 0; r < rows; r++)
+
     {
-	for (r = 0; r < rows; r++)
-        
-        {
-            
-            var square = document.createElement("div");
-		gameBoardContainer.appendChild(square);
-                
-            square.id = 's' + r + c;
-            
-            var topPosition = r * squareSize;
-            var leftPosition = c * squareSize;
-            
-            		square.style.top = topPosition + 'px';
-		square.style.left = leftPosition + 'px';						
-	}
-        console.log(gameBoardContainer);
-}
-        
-        var hitCount = 0;
-        
-        var gameBoard = [                                       
-				[ 0 , 0 , 0 , 1 , 1 , 1 , 1 , 0 , 0 , 0 ] ,          
-				[ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ] ,
-				[ 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ] ,
-				[ 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 ] ,
-				[ 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 ] ,
-				[ 1 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 , 0 ] ,
-				[ 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ] ,
-				[ 1 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 ] ,
-				[ 1 , 0 , 0 , 1 , 0 , 0 , 0 , 0 , 0 , 0 ] ,
-				[ 1 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 1 ]
-                                                                                ]
-                                
-        gameBoardContainer.addEventListener("click", shoot);
-        
-        console.log(gameBoard);
-        
-        
-        function shoot(event) {
-            if (event.target !== event.currentTarget){
-                
-                var row = event.target.id.substring(1,2);
-                
-                var col = event.target.id.substring(2,3) ; 
-                
-                   
-                    console.log(event.target);
-                    
-                
-                if (gameBoard[row][col] == 0) {
-			event.target.style.background = 'blue';
-            
-                        gameBoard[row][col] = 3;
-                        
-                    } else if (gameBoard[row][col] == 1) {
-			event.target.style.background = 'red';
-                        
-                        gameBoard[row][col] = 2;
-                        hitCount++;
-                        
-                        if (hitCount == 17) {
-				alert(" You have sunk all ships!");
-			}
-                    } 	
+
+        var square = document.createElement("div");
+        gameBoardContainer.appendChild(square);
+
+        square.id = 's' + r + c;
+
+        var topPosition = r * squareSize;
+        var leftPosition = c * squareSize;
+
+        square.style.top = topPosition + 'px';
+        square.style.left = leftPosition + 'px';
     }
-    
+    console.log(gameBoardContainer);
+}
+var hitCount = 0;
+
+var gameBoard = [
+    [0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 1, 1, 1]
+]
+
+gameBoardContainer.addEventListener("click", shoot);
+
+console.log(gameBoard);
+
+function shoot(event) {
+    if (event.target !== event.currentTarget) {
+
+        var row = event.target.id.substring(1, 2);
+
+        var col = event.target.id.substring(2, 3);
+
+        console.log(event.target);
+
+        if (gameBoard[row][col] == 0) {
+            event.target.style.background = 'blue';
+
+            gameBoard[row][col] = 3;
+
+        } else if (gameBoard[row][col] == 1) {
+            event.target.style.background = 'red';
+
+            gameBoard[row][col] = 2;
+            hitCount++;
+
+            if (hitCount == 17) {
+                alert(" You have sunk all ships!");
+            }
+        }
+    }
+
     e.stopPropagation();
 }
-        
+
         
